2d8294dbea74ce63aa99350d68aeac8513581d66
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-05 10:09:18 -0600

    isolated common funcs

---
+++ osext/env.go
@@ -0,0 +1,14 @@
+package osext
+
+import (
+  "log"
+  "os"
+)
+
+func MustGetenv(k string) string {
+  v := os.Getenv(k)
+  if v == "" {
+    log.Panicf("%s environment variable not set.", k)
+  }
+  return v
+}
