b8a1d3cb8b7a828394c94e9df3cd5f9ae7248b54
Zane Rockenbaugh <zane@liquid-labs.com>
2019-05-28 11:15:53 -0500

    expanding osext->env

--- osext/env.go
+++ osext/env.go
@@ -1,14 +1,44 @@
-package osext
+package env
 
 import (
   "log"
   "os"
 )
 
-func MustGetenv(k string) string {
+TODO: change this project to 'env'; the 'sqldb' stuff that was here will get factored out to catsql.
+
+func MustGet(k string) string {
   v := os.Getenv(k)
   if v == "" {
     log.Panicf("%s environment variable not set.", k)
   }
   return v
+}
+
+func Get(k string) string { return os.Getenv(k) }
+
+const environmentKey = `NODE_ENV`
+
+func MustGetType() {
+  return MustGet(environmentKey)
+}
+
+func GetType() {
+  return Get(environmentKey)
+}
+
+func IsDev() bool {
+  return GetEnv() == `dev`
+}
+
+func IsTest() bool {
+  return GetEnv() == `test`
+}
+
+func IsProduction() bool {
+  return GetEnv() == `produciton`
+}
+
+func IsStandardType() bool {
+  return IsDev() || IsTest() || IsProduction()
 }
